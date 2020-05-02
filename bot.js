28a28de5317b244a858e93631638abe86a33eaf2
Makhuta <matykubla@gmail.com>
2020-05-02 23:32:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,14 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NzAxNDIwNTcxNjk4NzkwNDcw.Xp3DTA.lTxfgBdhJj0u44kJK4JW5ER1uww';
-
-const PREFIX = '!';
-
 bot.on('ready', () =>{
     console.log('Ready!');
 })
 
 bot.on('message', msg=>{
-
-    let args = msg.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0]){
-        case 'test':
-            msg.channel.send('Tohle je test');
-            break;
-        case 'creator':
-            msg.channel.send('https://www.youtube.com/Makhuta')
-            break;
-        
+    if(msg.content === 'ping') {
+        msg.channel.send('Pong');       
     }
-})
+});
 
-bot.login(token);
+bot.login(process.env.BOT_TOKEN);
