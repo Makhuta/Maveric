7148f2075e4c9f58e9e49bdc349621c979efa206
Matěj <matykubla@gmail.com>
2020-05-03 20:17:31 +0200

    master

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const bot = new Discord.Client();
 bot.on("ready", async () =>{
     console.log('Ready!');
     bot.user.setActivity("Just normal day at Work.")
-})
+});
 
 bot.on("message", async message => {
     if (message.author.bot) return;
