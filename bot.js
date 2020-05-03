8686a554b861d4cd4f4dea4dfcda47c58793f863
Matěj <matykubla@gmail.com>
2020-05-03 21:16:44 +0200

    master

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 
-const PREFIX = botconfig.prefix;
+const PREFIX = '!';
 
 bot.on('ready', () =>{
     console.log('Ready!');
