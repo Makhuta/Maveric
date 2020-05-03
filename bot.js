c16e15b98be9f597b576897d16e413d5c0b5b6c9
Matěj <matykubla@gmail.com>
2020-05-03 21:26:07 +0200

    master

--- bot.js
+++ bot.js
@@ -24,35 +24,11 @@ bot.on('message', msg=>{
             msg.channel.send('https://www.youtube.com/Makhuta')
             break;
         case 'botinfo':
-            msg.channel.send({embed: {
-                color: 3447003,
-                author: {
-                  name: client.user.username,
-                  icon_url: client.user.avatarURL
-                },
-                title: "This is an embed",
-                url: "http://google.com",
-                description: "This is a test embed to showcase what they look like and what they can do.",
-                fields: [{
-                    name: "Fields",
-                    value: "They can have different fields with small headlines."
-                  },
-                  {
-                    name: "Masked links",
-                    value: "You can put [masked links](http://google.com) inside of rich embeds."
-                  },
-                  {
-                    name: "Markdown",
-                    value: "You can put all the *usual* **__Markdown__** inside of them."
-                  }
-                ],
-                timestamp: new Date(),
-                footer: {
-                  icon_url: client.user.avatarURL,
-                  text: "© Example"
-                }
-              }
-            });
+            let botembed = new Discord.RichEmbed()
+            .setDescription("Bot Information")
+            .setColor("#15f153")
+            .addField("Bot Name", bot.user.username);
+            msg.channel.send(botembed);
             break;
     }
 })
