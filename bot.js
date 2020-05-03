f374b8eeedda9f6c8dfe548f46460f89a0d5043c
Matěj <matykubla@gmail.com>
2020-05-03 21:05:23 +0200

    master

--- bot.js
+++ bot.js
@@ -19,19 +19,34 @@ bot.on("message", async message => {
 
     switch (cmd) {
         case 'botinfo':
-            message.channel.send({
-                embed: {
-                    color: ff1100,
-                    author: {
-                        name: client.user.username,
-                        icon_url: client.user.avatarURL
-                    },
-                    title: "Bot Information",
-                    fields: [{
-                        name: "Bot Name",
-                        value: `${bot.user.username}`
-                    }]
+            message.channel.send({embed: {
+                color: 3447003,
+                author: {
+                  name: client.user.username,
+                  icon_url: client.user.avatarURL
+                },
+                title: "This is an embed",
+                url: "http://google.com",
+                description: "This is a test embed to showcase what they look like and what they can do.",
+                fields: [{
+                    name: "Fields",
+                    value: "They can have different fields with small headlines."
+                  },
+                  {
+                    name: "Masked links",
+                    value: "You can put [masked links](http://google.com) inside of rich embeds."
+                  },
+                  {
+                    name: "Markdown",
+                    value: "You can put all the *usual* **__Markdown__** inside of them."
+                  }
+                ],
+                timestamp: new Date(),
+                footer: {
+                  icon_url: client.user.avatarURL,
+                  text: "© Example"
                 }
+              }
             });
             break;
     }
