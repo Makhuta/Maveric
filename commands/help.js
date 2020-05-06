8c0ddaa062b9352de0767bf06cfdab2769047f0b
Matěj <matykubla@gmail.com>
2020-05-06 22:08:12 +0200

    master

--- commands/help.js
+++ commands/help.js
@@ -4,5 +4,27 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-    name: "help"
+    name: "help",
+    aliases: ["h"],
+    category: "info",
+    description: "Returns all commands, or one specific command info",
+    usage: "[command | alias]",
+    run: async (bot, message, args) => {
+        getAll(client, message);
+    }
+}
+function getAll(bot, message) {
+    const embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+
+    const commands = (category) => {
+        return bot.commands
+        .filter(cmd => cmd.category === category)
+        .map(cmd => `- \`${cmd.name}\``)
+        .join("\n");
+    }
+    const info = client.categories
+    .map(cat => stripIndents`**${cat[0].coUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
+    .reduce((string, category) => string + "\n" + category);
+    return message.channel.send(embed.setDescription(info));
 }
