315de8493cb38cc94189b8336ec760e692d5951a
Matěj <matykubla@gmail.com>
2020-05-06 22:20:23 +0200

    master

--- commands/help.js
+++ commands/help.js
@@ -1,45 +1,68 @@
-module.exports.run = async(bot, message, args) => {
-    fs.readdir("./comamnds/", (err, files) => {
-        if(err) console.error(err);
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
 
-        let jsfiles = files.filter(f => f.split(".").pop() === "js");
-        if(jsfiles.length <= 0) {
-            console.log("No commands to load!");
-            return;
-        }
-
-
-        let result = jsfiles.forEach((f, i) => {
-            let props = require(`./${f}`);
-            let filesArray = [props.help.name, props.help.description, props.help.usage]
-            message.author.send(`**${filesArray[0]}** \n${filesArray[1]} \n${filesArray[2]}`);
-        });
-
-    });
-}
-
-module.exports.help = {
+module.exports = {
     name: "help",
     aliases: ["h"],
     category: "info",
     description: "Returns all commands, or one specific command info",
     usage: "[command | alias]",
-    /*run: async (bot, message, args) => {
-        getAll(client, message);*/
+    run: async (bot, message, args) => {
+        // If there's an args found
+        // Send the info of that command found
+        // If no info found, return not found embed.
+        if (args[0]) {
+            return getCMD(bot, message, args[0]);
+        } else {
+            // Otherwise send all the commands available
+            // Without the cmd info
+            return getAll(bot, message);
+        }
     }
-/*}
-function getAll(bot, message) {
-    const embed = new Discord.MessageEmbed()
-    .setColor("RANDOM")
+}
 
+function getAll(bot, message) {
+    const embed = new RichEmbed()
+        .setColor("RANDOM")
+        
+    // Map all the commands
+    // with the specific category
     const commands = (category) => {
         return bot.commands
-        .filter(cmd => cmd.category === category)
-        .map(cmd => `- \`${cmd.name}\``)
-        .join("\n");
+            .filter(cmd => cmd.category === category)
+            .map(cmd => `- \`${cmd.name}\``)
+            .join("\n");
     }
-    const info = client.categories
-    .map(cat => stripIndents`**${cat[0].coUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
-    .reduce((string, category) => string + "\n" + category);
+
+    // Map all the categories
+    const info = bot.categories
+        .map(cat => stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
+        .reduce((string, category) => string + "\n" + category);
+
     return message.channel.send(embed.setDescription(info));
-}*/
+}
+
+function getCMD(bot, message, input) {
+    const embed = new RichEmbed()
+
+    // Get the cmd by the name or alias
+    const cmd = bot.commands.get(input.toLowerCase()) || bot.commands.get(bot.aliases.get(input.toLowerCase()));
+    
+    let info = `No information found for command **${input.toLowerCase()}**`;
+
+    // If no cmd is found, send not found embed
+    if (!cmd) {
+        return message.channel.send(embed.setColor("RED").setDescription(info));
+    }
+
+    // Add all cmd info to the embed
+    if (cmd.name) info = `**Command name**: ${cmd.name}`;
+    if (cmd.aliases) info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(", ")}`;
+    if (cmd.description) info += `\n**Description**: ${cmd.description}`;
+    if (cmd.usage) {
+        info += `\n**Usage**: ${cmd.usage}`;
+        embed.setFooter(`Syntax: <> = required, [] = optional`);
+    }
+
+    return message.channel.send(embed.setColor("GREEN").setDescription(info));
+}
