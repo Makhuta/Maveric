0d951fbd71aa8d78173685e289e2df573b0b2576
Matěj <matykubla@gmail.com>
2020-05-06 22:12:35 +0200

    master

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,29 @@
+const fs = require("fs");
 const Discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
 
+module.exports.run = async(bot, message, args) => {
+    fs.readdir("./cmds/", (err, files) => {
+        if(err) console.error(err);
+
+        let jsfiles = files.filter(f => f.split(".").pop() === "js");
+        if(jsfiles.length <= 0) {
+            console.log("No commands to load!");
+            return;
+        }
+
+        var namelist = "";
+        var desclist = "";
+        var usage = "";
+
+        let result = jsfiles.forEach((f, i) => {
+            let props = require(`./${f}`);
+            namelist = props.help.name;
+            desclist = props.help.description;
+            usage = props.help.usage;
+        });
+
+        message.author.send(`**${namelist}** \n${desclist} \n${usage}`);
+    });
 }
 
 module.exports.help = {
@@ -9,10 +32,10 @@ module.exports.help = {
     category: "info",
     description: "Returns all commands, or one specific command info",
     usage: "[command | alias]",
-    run: async (bot, message, args) => {
-        getAll(client, message);
+    /*run: async (bot, message, args) => {
+        getAll(client, message);*/
     }
-}
+/*}
 function getAll(bot, message) {
     const embed = new Discord.MessageEmbed()
     .setColor("RANDOM")
@@ -27,4 +50,4 @@ function getAll(bot, message) {
     .map(cat => stripIndents`**${cat[0].coUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
     .reduce((string, category) => string + "\n" + category);
     return message.channel.send(embed.setDescription(info));
-}
+}*/
