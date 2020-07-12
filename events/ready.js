6cf2986403dee7deec0beff864da6fb3adc3f80d
Matěj <matykubla@gmail.com>
2020-07-12 03:42:20 +0200

    master

--- events/ready.js
+++ events/ready.js
@@ -4,5 +4,5 @@ const botconfig = require("../botconfig.json")
 bot.login(process.env.BOT_TOKEN);
 bot.on("ready", () => {
     console.log(`${bot.user.username} is Ready!`);
-    bot.user.setActivity({type: "WATCHING"}, 'NSBR Server');
+    bot.user.setActivity('NSBR Server', {type: "WATCHING"});
   })
