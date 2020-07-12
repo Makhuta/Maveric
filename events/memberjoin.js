1b1ae0b2df17aa6f9dda2f6e79d962f5850cd917
Matěj <matykubla@gmail.com>
2020-07-12 16:32:38 +0200

    update 1.3

---
+++ events/memberjoin.js
@@ -0,0 +1,28 @@
+const { bot } = require('../bot');
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const { Guild } = require('discord.js');
+const color = require("../colors.json")
+const Canvas = require('canvas')
+
+bot.on("guildMemberAdd", (member) => {
+    bot.channels.fetch(bot.channels.cache.find(c => c.name === botconfig.gateroom).id)
+        .then(channel => {
+            var d = new Date(member.joinedTimestamp)
+            var url = member.user.displayAvatarURL({ format: "png", size: 512 })
+            var boturl = bot.user.displayAvatarURL({ format: "png", size: 512 })
+            const msg = bot.channels.cache.get(channel.id)
+            let welcomemsg = new Discord.MessageEmbed()
+                .setTitle(`Vítej ${member.user.username}`)
+                .setColor(color.red)
+                .setDescription(`
+            **Nový člen smečky**
+            Právě se k nám přidal **${member.user.username}**
+            Discord účet si založil: **${d.toLocaleString()}**
+            `)
+                .setThumbnail(url)
+                .setTimestamp()
+                .setFooter(bot.user.username, boturl)
+            msg.send({ embed: welcomemsg });
+        })
+})
---
+++ events/memberleave.js
@@ -0,0 +1,27 @@
+const { bot } = require('../bot');
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const { Guild } = require('discord.js');
+const color = require("../colors.json")
+
+bot.on("guildMemberRemove", (member) => {
+    bot.channels.fetch(bot.channels.cache.find(c => c.name === botconfig.gateroom).id)
+        .then(channel => {
+            var d = new Date(member.joinedTimestamp)
+            var url = member.user.displayAvatarURL({ format: "png", size: 512 })
+            var boturl = bot.user.displayAvatarURL({ format: "png", size: 512 })
+            const msg = bot.channels.cache.get(channel.id)
+            let welcomemsg = new Discord.MessageEmbed()
+                .setTitle(`Zrádce ${member.user.username}`)
+                .setColor(color.red)
+                .setDescription(`
+            **Další zrádce**
+            Právě nás zradil **${member.user.username}**
+            Discord účet si založil: **${d.toLocaleString()}**
+            `)
+                .setThumbnail(url)
+                .setTimestamp()
+                .setFooter(bot.user.username, boturl)
+            msg.send({ embed: welcomemsg });
+        })
+})
