25f45e318030547f4a9e7b3674e2f77bc74ed967
Matěj <matykubla@gmail.com>
2020-07-19 17:43:50 +0200

    Botinfo Update

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,16 +1,22 @@
 const Discord = require("discord.js");
-const { prefix, botusername, creatorusername } = require("../botconfig.json")
+const { prefix, botusername, creatorusername, guildid, youtubeurl } = require("../botconfig.json")
 const color = require("../colors.json")
 module.exports.run = async (bot, message, args) => {
+
+    var boturl = bot.user.displayAvatarURL({ format: "png", size: 512 })
+    var servername = bot.guilds.cache.get(guildid).name
 
     let botembed = new Discord.MessageEmbed()
         .setTitle("Informace")
         .setColor(color.red)
-        .setDescription(`
-            - **Jméno bota** ${bot.user.username}
-            - **Bota vytvořil** ${creatorusername}
-            `)
-            .setFooter(bot.user.username)
+        .addFields(
+            { name: "**Jméno bota**", value: bot.user.username, inline: true },
+            { name: "**Bota vytvořil**", value: creatorusername, inline: true },
+            { name: "**Youtube channel autora**", value: `[Makhuta](${youtubeurl})`, inline: true },
+            { name: "**Účel bota**", value: `${bot.user.username} byl vytvořen na základě snížení náročnosti na správu ${servername} serveru a zajistit snazší interakci uživatelů se serverem.` }
+        )
+        .setFooter(bot.user.username, boturl)
+        .setTimestamp()
 
 
     message.channel.send({ embed: botembed });
