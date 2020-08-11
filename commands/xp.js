2a4e5ed0d2d1da451e0542982024ffb16be44813
Matěj <matykubla@gmail.com>
2020-08-11 19:16:09 +0200

    XP command Update

--- commands/xp.js
+++ commands/xp.js
@@ -9,9 +9,9 @@ const usage = `${botconfig.prefix}xp`
 const accessableby = ["Member"]
 const aliases = [""]
 
-function zprava(xp, level, message, Discord) {
+function zprava(xp, level, message, Discord, xpToNextLevel) {
     let embed = new Discord.MessageEmbed()
-    embed.addFields({name: "Statistiky uživatele "+ message.author.username, value: "Tvůj Level je " + level + "\nTvoje XP jsou " + xp})
+    embed.addFields({name: "Statistiky uživatele "+ message.author.username, value: "Tvůj Level je " + level + "\nTvoje XP jsou " + xp + "/" + xpToNextLevel})
     embed.setColor(color.red)
     message.channel.send(embed)
 }
@@ -25,7 +25,8 @@ con.query(`SELECT * FROM userstats WHERE id = '${target.id}'`, (err, rows) => {
     if(!rows[0]) return message.channel.send("This user has no XP on record.")
     let xp = rows[0].xp
     let level = rows[0].level
-    zprava(xp, level, message, Discord)
+    var xpToNextLevel = 5 * Math.pow(level, 2) + 50 * level + 100
+    zprava(xp, level, message, Discord, xpToNextLevel)
 })
 }
 
