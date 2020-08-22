9ef7c7e2f02efed14a67b9ab2dab29de97954fc2
Matěj <matykubla@gmail.com>
2020-08-22 10:23:33 +0200

    XP on message update

---
+++ events/lvl_on_message.js
@@ -0,0 +1,75 @@
+const { bot, con } = require('../bot');
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const { Guild } = require('discord.js');
+const color = require("../colors.json")
+const mysqlconfig = require("../mysqlconfig.json")
+const fs = require('fs')
+const mysql = require('mysql')
+const random = require('random')
+
+function generateXP() {
+    return random.int(10, 30)
+}
+
+function zprava(level, typek, message, Discord) {
+    let embed = new Discord.MessageEmbed()
+    embed.addFields({ name: "Level UP", value: typek + " právě postoupil do levlu " + level + "." })
+    embed.setColor(color.red)
+    message.channel.send(embed)
+}
+
+function databaze(message, con) {
+    con.query(`SELECT * FROM userstats WHERE id = '${message.author.id}'`, (err, rows) => {
+        if (err) throw err;
+        //console.log(err + "\n")
+
+        let sql
+        //console.log(`\n${rows}\n`)
+        if (rows.length < 1) {
+            //console.log("prvni")
+            sql = `INSERT INTO userstats (id, xp) VALUES ('${message.author.id}', ${generateXP()})`
+            con.query(sql)
+        }
+        else {
+            var xp = rows[0].xp
+            var level = rows[0].level
+            var lastmsg = rows[0].last_message
+            var cas = Date.now()
+
+            if (Date.now() - lastmsg > 60000) {
+                xp += generateXP()
+                var xpToNextLevel = 5 * Math.pow(level, 2) + 50 * level + 100
+                //console.log(xpToNextLevel)
+                if (xp >= xpToNextLevel) {
+                    level++;
+                    xp = xp - xpToNextLevel;
+                    zprava(level, message.author.username, message, Discord)
+
+                    sql = `UPDATE userstats SET xp = ${xp} WHERE id = '${message.author.id}'`;
+                    con.query(sql)
+                    sql = `UPDATE userstats SET level = ${level} WHERE id = '${message.author.id}'`;
+                    con.query(sql)
+                    sql = `UPDATE userstats SET last_message = ${cas} WHERE id = '${message.author.id}'`;
+                    con.query(sql)
+                }
+                else {
+                    sql = `UPDATE userstats SET xp = ${xp} WHERE id = '${message.author.id}'`, `UPDATE userstats SET last_message = ${cas} WHERE id = '${message.author.id}'`;
+                    con.query(sql)
+                    sql = `UPDATE userstats SET last_message = ${cas} WHERE id = '${message.author.id}'`;
+                    con.query(sql)
+                }
+            }
+            else return
+            //console.log(level + " " + xp)
+        }
+
+        //console.log(rows)
+    })
+}
+
+bot.on("message", message => {
+    if (message.author.bot || message.channel.type === "dm" || message.content === botconfig.prefix + "xp") return
+    databaze(message, con);
+
+})
--- events/message.js
+++ events/message.js
@@ -1,34 +1,11 @@
 const Discord = require("discord.js")
-const { bot } = require('../bot');
+const { bot, con } = require('../bot');
 const botconfig = require("../botconfig.json")
 const mysqlconfig = require("../mysqlconfig.json")
 const mysql = require('mysql')
 const random = require('random')
 const color = require("../colors.json")
 const prefix = botconfig.prefix;
-
-var con = mysql.createPool({
-  host: mysqlconfig.host,
-  user: mysqlconfig.user,
-  password: mysqlconfig.password,
-  database: mysqlconfig.database
-});
-
-/*con.connect(err => {
-  if (err) throw err;
-  console.log("\nConnected to database!\n")
-});*/
-
-function generateXP() {
-  return random.int(10, 30)
-}
-
-function zprava(level, typek, message, Discord) {
-  let embed = new Discord.MessageEmbed()
-  embed.addFields({ name: "Level UP", value: typek + " právě postoupil do levlu " + level + "." })
-  embed.setColor(color.red)
-  message.channel.send(embed)
-}
 
 function prikaz(message) {
   let messageArray = message.content.split(" ");
@@ -55,60 +32,8 @@ function prikaz(message) {
   }
 }
 
-function databaze(message, con) {
-  con.query(`SELECT * FROM userstats WHERE id = '${message.author.id}'`, (err, rows) => {
-    if (err) throw err;
-    //console.log(err + "\n")
-
-    let sql
-    //console.log(`\n${rows}\n`)
-    if (rows.length < 1) {
-      //console.log("prvni")
-      sql = `INSERT INTO userstats (id, xp) VALUES ('${message.author.id}', ${generateXP()})`
-      con.query(sql)
-    }
-    else {
-      var xp = rows[0].xp
-      var level = rows[0].level
-      var lastmsg = rows[0].last_message
-      var cas = Date.now()
-
-      if (Date.now() - lastmsg > 60000) {
-        xp += generateXP()
-        var xpToNextLevel = 5 * Math.pow(level, 2) + 50 * level + 100
-        //console.log(xpToNextLevel)
-        if (xp >= xpToNextLevel) {
-          level++;
-          xp = xp - xpToNextLevel;
-          zprava(level, message.author.username, message, Discord)
-
-          sql = `UPDATE userstats SET xp = ${xp} WHERE id = '${message.author.id}'`;
-          con.query(sql)
-          sql = `UPDATE userstats SET level = ${level} WHERE id = '${message.author.id}'`;
-          con.query(sql)
-          sql = `UPDATE userstats SET last_message = ${cas} WHERE id = '${message.author.id}'`;
-          con.query(sql)
-        }
-        else {
-          sql = `UPDATE userstats SET xp = ${xp} WHERE id = '${message.author.id}'`, `UPDATE userstats SET last_message = ${cas} WHERE id = '${message.author.id}'`;
-          con.query(sql)
-          sql = `UPDATE userstats SET last_message = ${cas} WHERE id = '${message.author.id}'`;
-          con.query(sql)
-        }
-      }
-      else return
-      //console.log(level + " " + xp)
-    }
-
-    //console.log(rows)
-  })
-}
-
 bot.on("message", message => {
   if (message.author.bot || message.channel.type === "dm") return;
   prikaz(message);
-
-  if (message.content === prefix + "xp") return
-  databaze(message, con);
 
 })
