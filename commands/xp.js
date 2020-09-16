2f4d64b9f5938e6cc0e98adea98f112c94456fc1
Makhuta <matykubla@gmail.com>
2020-09-17 00:57:20 +0200

    XP Update

--- commands/xp.js
+++ commands/xp.js
@@ -1,11 +1,11 @@
-const { bot, con } = require('../bot');
-const { MessageAttachment } = require("discord.js");
+const Discord = require("discord.js");
 const botconfig = require("../botconfig.json")
 const color = require("../colors.json")
 const fs = require("fs");
 const { createCanvas, loadImage, Canvas } = require("canvas");
 const { join } = require("path");
 const { createGzip } = require("zlib");
+const canvasxp = require("../funkce/canvasxp")
 
 const name = "xp"
 const description = `Vypíše počet XP`
@@ -13,117 +13,53 @@ const usage = `${botconfig.prefix}xp`
 const accessableby = ["Member"]
 const aliases = [""]
 
-async function statistika(xp, level, target, message, xpToNextLevel, rank) {
-    const canvas = createCanvas(1000, 333);
-    const ctx = canvas.getContext('2d');
-    const background = await loadImage(join(__dirname, "..", "pictures", "background.jpg"));
-    var sirka = ((100 / (xpToNextLevel)) * xp) * 7.7;
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-    ctx.beginPath();
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = "#ffffff";
-    ctx.globalAlpha = 0.2;
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(180, 216, 770, 65);
-    ctx.fill();
-    ctx.globalAlpha = 1;
-    ctx.strokeRect(180, 216, 770, 65);
-    ctx.stroke();
-
-    ctx.globalAlpha = 0.6;
-    ctx.fillStyle = "#e67e22";
-    ctx.fillRect(180, 216, sirka, 65);
-    ctx.fill();
-    ctx.globalAlpha = 1;
-
-    ctx.font = "30px Arial";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "#ffffff";
-    ctx.fillText(`${xp}/${xpToNextLevel} XP`, 600, 260);
-
-    ctx.textAlign = "left"
-    ctx.fillText(target.username + " #" + target.discriminator, 300, 80);
-
-    ctx.font = "50px Arial";
-    ctx.fillText("Level: #" + level, 300, 130);
-
-    ctx.font = "50px Arial";
-    ctx.fillText("Rank: #" + rank, 300, 180);
-
-    ctx.arc(170, 160, 120, 0, Math.PI * 2, true);
-    ctx.lineWidth = 6;
-    ctx.strokeStyle = "#ffffff";
-    ctx.stroke();
-    ctx.closePath();
-    ctx.clip();
-    const avatar = await loadImage(target.displayAvatarURL({ format: "jpg" }));
-    ctx.drawImage(avatar, 40, 40, 250, 250);
-
-    const attachment = new MessageAttachment(canvas.toBuffer(), "rank.png");
-    message.channel.send(attachment)
-}
-
-function allxp(level, xp) {
-    var xpecka = xp
-    for (let l = 0; l < level; l++) {
-        var xpToNextLevel = 5 * Math.pow(l, 2) + 50 * l + 100;
-        xpecka = xpecka + xpToNextLevel;
-    }
-    /*sql = `UPDATE userstats SET allxp = ${xpecka} WHERE id = '${target.id}'`;
-    con.query(sql)*/
-    return (xpecka)
-}
-
-function getrank(xp, level, con, resid, resallxp, rank, target, message, xpToNextLevel) {
-    con.query(`SELECT id, xp, level FROM userstats`, function (err, result, fields) {
-        if (err) throw err;
-        var usraray = []
-        var reslength = result.length - 1
-        for (let d = 0; d <= reslength; d++) {
-            resid = result[d].id;
-            let reslevel = result[d].level;
-            let resxp = result[d].xp;
-            resallxp = allxp(reslevel, resxp, target)
-            usraray.push({id: resid, allxps: resallxp})
-        }
-        usraray.sort((a, b) => (a.allxps < b.allxps) ? 1 : (a.allxps === b.allxps) ? ((a.id < b.id) ? 1 : -1) : -1 )
-        
-        for (let d = 0; d <= reslength; d++) {
-            if (usraray[d].id === target.id) {
-                rank = d + 1
-                //console.log(resid + " " + resallxp + " #" + rank)
-
-                //console.log(usraray)
-                statistika(xp, level, target, message, xpToNextLevel, rank)
-            }
-
-        }
-       
-
-    });
-}
 
 module.exports.run = async (bot, message, args, con) => {
     let target = message.mentions.users.first() || message.guild.members.cache.get(args[1]) || message.author;
-    var rank
-    var resid
-    var resallxp
-
 
 
     con.query(`SELECT * FROM userstats WHERE id = '${target.id}'`, (err, rows) => {
         if (err) throw err;
+        var test = con.query(`SELECT id FROM userstats`, (err, rows))
+        var test_values = test.values
+        var test_level = []
+        var test_xp = []
+
+        con.query("SELECT id, level, xp FROM userstats", function (err, result, fields) {
+            if (err) throw err;
+            var aray = JSON.stringify(result)
+            var araysplit = aray.split(",")
+            //console.log(aray)
+            /*Object.keys(result).forEach(function(key) {
+                var row = result[key]
+                console.log(id);
+            })*/
 
 
+        });
+
+        /* var queries = {
+             users: 'SELECT * FROM userstats'
+         };
+         
+         const getList = (queryName, queryParams) => {
+             return new Promise(function(resolve, reject){
+                 con.query(queries[queryName], queryParams, function(id, err, result, fields){
+                     if (!err) resolve(JSON.parse(JSON.stringify(result))); // Hacky solution
+                     else reject(err);
+                     //console.log(resolve(JSON.parse(JSON.stringify(result))))
+                 });
+             });
+         };
+ 
+         getList("id")*/
 
         if (!rows[0]) return message.channel.send("This user has no XP on record.")
         let xp = rows[0].xp
         let level = rows[0].level
-        let sql
         var xpToNextLevel = 5 * Math.pow(level, 2) + 50 * level + 100
-        allxp(level, xp, sql, target)
-        getrank(xp, level, con, resid, resallxp, rank, target, message, xpToNextLevel, sql)
+        canvasxp.execute(xp, level, target, message, xpToNextLevel)
+
     })
 }
 
---
+++ funkce/canvasxp.js
@@ -0,0 +1,55 @@
+const { MessageAttachment } = require("discord.js");
+const { createCanvas, loadImage, Canvas } = require("canvas");
+const { join } = require("path");
+
+module.exports = {
+    name: "canvasxp",
+    async execute(xp, level, target, message, xpToNextLevel) {
+        const canvas = createCanvas(1000, 333);
+    const ctx = canvas.getContext('2d');
+    const background = await loadImage(join(__dirname, "..", "pictures", "background.jpg"));
+    var sirka = ((100 / (xpToNextLevel)) * xp) * 7.7;
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = "#ffffff";
+    ctx.globalAlpha = 0.2;
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(180, 216, 770, 65);
+    ctx.fill();
+    ctx.globalAlpha = 1;
+    ctx.strokeRect(180, 216, 770, 65);
+    ctx.stroke();
+
+    ctx.globalAlpha = 0.6;
+    ctx.fillStyle = "#e67e22";
+    ctx.fillRect(180, 216, sirka, 65);
+    ctx.fill();
+    ctx.globalAlpha = 1;
+
+    ctx.font = "30px Arial";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(`${xp}/${xpToNextLevel} XP`, 600, 260);
+
+    ctx.textAlign = "left"
+    ctx.fillText(target.username + "#" + target.discriminator, 300, 120);
+
+    ctx.font = "50px Arial";
+    ctx.fillText("Level:", 300, 180);
+    ctx.fillText(level, 470, 180);
+
+    ctx.arc(170, 160, 120, 0, Math.PI * 2, true);
+    ctx.lineWidth = 6;
+    ctx.strokeStyle = "#ffffff";
+    ctx.stroke();
+    ctx.closePath();
+    ctx.clip();
+    const avatar = await loadImage(target.displayAvatarURL({ format: "jpg" }));
+    ctx.drawImage(avatar, 40, 40, 250, 250);
+
+    const attachment = new MessageAttachment(canvas.toBuffer(), "rank.png");
+    message.channel.send(attachment)
+    }
+}
