b29d8e40a0ba0a5f3988f0bbcf40bf22aa38a4b2
Makhuta <matykubla@gmail.com>
2020-09-20 16:23:03 +0200

    XP Update

--- funkce/canvasxp.js
+++ funkce/canvasxp.js
@@ -3,52 +3,54 @@ const { createCanvas, loadImage, Canvas } = require("canvas");
 const { join } = require("path");
 
 module.exports = {
-    async run(xp, level, target, message, xpToNextLevel) {
+    async run(xp, level, target, message, xpToNextLevel, rank) {
         const canvas = createCanvas(1000, 333);
-    const ctx = canvas.getContext('2d');
-    const background = await loadImage(join(__dirname, "..", "pictures", "background.jpg"));
-    var sirka = ((100 / (xpToNextLevel)) * xp) * 7.7;
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        const ctx = canvas.getContext('2d');
+        const background = await loadImage(join(__dirname, "..", "pictures", "background.jpg"));
+        var sirka = ((100 / (xpToNextLevel)) * xp) * 7.7;
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-    ctx.beginPath();
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = "#ffffff";
-    ctx.globalAlpha = 0.2;
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(180, 216, 770, 65);
-    ctx.fill();
-    ctx.globalAlpha = 1;
-    ctx.strokeRect(180, 216, 770, 65);
-    ctx.stroke();
+        ctx.beginPath();
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = "#ffffff";
+        ctx.globalAlpha = 0.2;
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(180, 216, 770, 65);
+        ctx.fill();
+        ctx.globalAlpha = 1;
+        ctx.strokeRect(180, 216, 770, 65);
+        ctx.stroke();
 
-    ctx.globalAlpha = 0.6;
-    ctx.fillStyle = "#e67e22";
-    ctx.fillRect(180, 216, sirka, 65);
-    ctx.fill();
-    ctx.globalAlpha = 1;
+        ctx.globalAlpha = 0.6;
+        ctx.fillStyle = "#e67e22";
+        ctx.fillRect(180, 216, sirka, 65);
+        ctx.fill();
+        ctx.globalAlpha = 1;
 
-    ctx.font = "30px Arial";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "#ffffff";
-    ctx.fillText(`${xp}/${xpToNextLevel} XP`, 600, 260);
+        ctx.font = "30px Arial";
+        ctx.textAlign = "center";
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(`${xp}/${xpToNextLevel} XP`, 600, 260);
 
-    ctx.textAlign = "left"
-    ctx.fillText(target.username + "#" + target.discriminator, 300, 120);
+        ctx.textAlign = "left"
+        ctx.fillText(target.username + "#" + target.discriminator, 300, 80);
 
-    ctx.font = "50px Arial";
-    ctx.fillText("Level:", 300, 180);
-    ctx.fillText(level, 470, 180);
+        ctx.font = "50px Arial";
+        ctx.fillText("Level: #" + level, 300, 130);
 
-    ctx.arc(170, 160, 120, 0, Math.PI * 2, true);
-    ctx.lineWidth = 6;
-    ctx.strokeStyle = "#ffffff";
-    ctx.stroke();
-    ctx.closePath();
-    ctx.clip();
-    const avatar = await loadImage(target.displayAvatarURL({ format: "jpg" }));
-    ctx.drawImage(avatar, 40, 40, 250, 250);
+        ctx.font = "50px Arial";
+        ctx.fillText("Rank: #" + rank, 300, 180);
 
-    const attachment = new MessageAttachment(canvas.toBuffer(), "rank.png");
-    message.channel.send(attachment)
+        ctx.arc(170, 160, 120, 0, Math.PI * 2, true);
+        ctx.lineWidth = 6;
+        ctx.strokeStyle = "#ffffff";
+        ctx.stroke();
+        ctx.closePath();
+        ctx.clip();
+        const avatar = await loadImage(target.displayAvatarURL({ format: "jpg" }));
+        ctx.drawImage(avatar, 40, 40, 250, 250);
+
+        const attachment = new MessageAttachment(canvas.toBuffer(), "rank.png");
+        message.channel.send(attachment)
     }
 }
---
+++ funkce/getrank.js
@@ -0,0 +1,48 @@
+var rank
+var resid
+var resallxp
+
+function allxp(level, xp) {
+    var xpecka = xp
+    for (let l = 0; l < level; l++) {
+        var xpToNextLevel = 5 * Math.pow(l, 2) + 50 * l + 100;
+        xpecka = xpecka + xpToNextLevel;
+    }
+    /*sql = `UPDATE userstats SET allxp = ${xpecka} WHERE id = '${target.id}'`;
+    con.query(sql)*/
+    return (xpecka)
+}
+
+async function output(rank, callfunction) {
+    await callfunction(rank)
+}
+
+module.exports = {
+    run: async (xp, level, con, target, message, xpToNextLevel, callfunction) => {
+        con.query(`SELECT id, xp, level FROM userstats`, function (err, result, fields) {
+            if (err) throw err;
+            var usraray = []
+            var reslength = result.length - 1
+            for (let d = 0; d <= reslength; d++) {
+                resid = result[d].id;
+                let reslevel = result[d].level;
+                let resxp = result[d].xp;
+                resallxp = allxp(reslevel, resxp, target)
+                usraray.push({ id: resid, allxps: resallxp })
+            }
+            usraray.sort((a, b) => (a.allxps < b.allxps) ? 1 : (a.allxps === b.allxps) ? ((a.id < b.id) ? 1 : -1) : -1)
+
+            for (let d = 0; d <= reslength; d++) {
+                if (usraray[d].id === target.id) {
+                    rank = d + 1
+                    //console.log(resid + " " + resallxp + " #" + rank)
+
+                    callfunction(xp, level, target, message, xpToNextLevel, rank)
+                }
+
+            }
+
+
+        });
+    }
+}
