c88e6ceeaec0cd7884e3863f461e111acc6673ad
Makhuta <matykubla@gmail.com>
2020-10-08 10:16:35 +0200

    Member join fix Date

--- events/memberjoin.js
+++ events/memberjoin.js
@@ -19,7 +19,7 @@ function zprava(level, typek, message, Discord) {
 function uvitani(member) {
     bot.channels.fetch(bot.channels.cache.find(c => c.name === roomnames.gateroom).id)
         .then(channel => {
-            var d = new Date(member.joinedTimestamp).toLocaleDateString('en').split("/")
+            var d = new Date(member.guild.joinedTimestamp).toLocaleDateString('en').split("/")
             var datum = [d[1], d[0], d[2]].join(". ")
             const msg = bot.channels.cache.get(channel.id)
             let hodnoty = ({ createCanvas: createCanvas, message: msg, join: join, MessageAttachment: MessageAttachment, loadImage: loadImage, color: color, target: member.user, stav: "Welcome", datum: datum })
