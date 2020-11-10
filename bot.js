ed8cde7f522ce622b4f4e648ec386bbb571d46f7
Makhuta <matykubla@gmail.com>
2020-11-10 17:33:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,20 +2,23 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({ disableMentions: "everyone", ws: { intents: Discord.Intents.ALL } });
 const mysql = require('mysql')
 const WS = require("./handlers/ws/ws")
+const mysqlconfig = require("./mysqlconfig.json")
 require("./functions")(bot);
 
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 bot.accessable = new Discord.Collection();
 
+
+
 var con = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  host: mysqlconfig.host,
+  user: mysqlconfig.user,
+  password: mysqlconfig.password,
+  database: mysqlconfig.database
 });
 
-var ws = new WS("123456", 5665, bot, con)
+var ws = new WS("statistics", process.env.PORT || 5665, bot, con)
 
 module.exports = {
   bot: bot,
