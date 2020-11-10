5c0da5b1bdfe2c0324530d20a5e624dd724fde12
Makhuta <matykubla@gmail.com>
2020-11-10 18:02:12 +0100

    Update ws.js

--- handlers/ws/ws.js
+++ handlers/ws/ws.js
@@ -53,37 +53,19 @@ class WebSocket {
         this.app.get("/", (req, res) => {
             var _token = req.query.token
 
-            if (!this.checkToken(_token)) {
+            /*if (!this.checkToken(_token)) {
                 res.render('error', { title: "ERROR", errtype: "INVALID TOKEN" })
                 return
-            }
+            }*/
 
             var mems = [{id: "ID", xp: "XP", username: "USERNAME", level: "LEVEL", discriminator: "DISCRIMINATOR"}]
             statistiky(mems, this.con, this.bot)
 
             res.render("index", {
                 title: "discordBot webinterface",
-                token: _token,
                 mems
             })
         })
-
-        this.app.post("/sendMessage", async (req, res) => {
-            var _token = req.body.token
-            var text = req.body.text
-            var channelid = req.body.channelid
-
-            if (!this.checkToken(_token))
-                return
-
-            var chan = await this.bot.guilds.cache.first().channels.cache.get(channelid)
-            console.log(channelid)
-            if (chan) {
-                console.log(chan)
-                chan.send(text)
-            }
-        })
-
     }
 
 }
