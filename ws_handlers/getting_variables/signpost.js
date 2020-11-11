5a30d8d4609f24fcad232736ee68c531f6d3c5ba
Makhuta <matykubla@gmail.com>
2020-11-11 14:13:22 +0100

    Update Website

--- ws_handlers/getting_variables/signpost.js
+++ ws_handlers/getting_variables/signpost.js
@@ -1,5 +1,31 @@
+const fs = require("fs");
+
 module.exports = {
-    async run (token) {
-        return await require(`./${token}`).run()
+    async run(token) {
+        let exist = false
+        fs.readdir("./", async (err, files) => {
+            let out_name
+            if (err) console.log(err);
+
+            let jsfile = files.find(f => f.split(".").pop() === "js");
+            if (jsfile.length <= 0) {
+                return;
+            }
+
+
+            if (jsfile != token) {
+                exist = exist
+            }
+
+            else {
+                exist = true
+            }
+        });
+
+        if (!exist) return
+
+        let output = await require(`./${token}`)
+        console.log(output.run())
+        return output
     }
 }
