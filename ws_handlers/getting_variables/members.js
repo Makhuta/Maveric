7e394232c9122074b21a4612b0dc0eed12a248a8
Makhuta <matykubla@gmail.com>
2020-11-12 00:46:17 +0100

    Update members.js

--- ws_handlers/getting_variables/members.js
+++ ws_handlers/getting_variables/members.js
@@ -1,13 +1,6 @@
 const { use } = require("random")
 const { bot, con } = require("../../bot")
 
-
-
-function toarray(list, users) {
-    users.push(list)
-}
-
-
 module.exports = {
     async run(hodnoty) {
         let res = hodnoty.res
@@ -20,44 +13,28 @@ module.exports = {
         var xpToNextLevel = 0
         var users = new Array()
 
-
-
         bot.users.cache.filter(u => !u.bot).forEach(async (user) => {
-            let output
-
             users.push({ id: user.id, username: user.username, discriminator: user.discriminator, xp: 0, level: 0, xpToNextLevel: 0 })
-            await con.query(`SELECT * FROM userstats WHERE id = '${user.id}'`, async (err, rows) => {
-                if (err)
-                    throw err
+        })
 
-                if (!rows[0])
-                    return
-                xp = rows[0].xp
-                level = rows[0].level
+        con.query(`SELECT * FROM userstats`, async (err, rows) => {
+            if (err)
+                throw err
+
+            rows.forEach(async (user) => {
+                xp = user.xp
+                level = user.level
                 xpToNextLevel = 5 * Math.pow(level, 2) + 50 * level + 100
-                output = "TEST"//{ id: user.id, username: user.username, discriminator: user.discriminator, xp: xp, level: level, xpToNextLevel: xpToNextLevel }
-
-                /* users.push({
-                     id: user.id,
-                     username: user.username,
-                     discriminator: user.discriminator,
-                     xp: xp,
-                     level: level,
-                     xpToNextLevel: xpToNextLevel
-                 })*/
-                //await users.push(output)
                 let get_user = users.find(u => u.id == user.id)
                 get_user.xp = xp
                 get_user.level = level
                 get_user.xpToNextLevel = xpToNextLevel
-                return users
+
             })
-            console.log(users)
         });
 
-        console.log(users)
         res.render(view_hbs, { title: title, host_value: host_value, user: users });
-        console.log(users)
+
 
 
     }
