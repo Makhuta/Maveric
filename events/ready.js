eff1f11ce0be75b8988b9d86f475aec8528fb069
Makhuta <matykubla@gmail.com>
2021-01-18 22:04:55 +0100

    env update

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+require("dotenv").config();
 const { bot } = require('../bot');
 const roomnames = require("../botconfig/roomnames.json")
 const emojinames = require("../botconfig/emojinames.json")
@@ -8,12 +9,12 @@ const examplereaction = require("../handlers/verification/reaction")
 
 bot.login(process.env.BOT_TOKEN);
 bot.on("ready", () => {
-  console.log(`${bot.user.username} is Ready!`);
-  let hodnoty = ({ bot: bot, verifyroomname: roomnames.verifyroom, color: color, discord: Discord, verifyemojiname: emojinames.verifyemojiname })
-  verifymessage.run(hodnoty)
+    console.log(`${bot.user.username} is Ready!`);
+    let hodnoty = ({ bot: bot, verifyroomname: roomnames.verifyroom, color: color, discord: Discord, verifyemojiname: emojinames.verifyemojiname })
+    verifymessage.run(hodnoty)
 })
 
 bot.on("message", async message => {
-  let hodnoty = ({ bot: bot, verifyroomname: roomnames.verifyroom, verifyemojiname: emojinames.verifyemojiname, message: message })
-  examplereaction.run(hodnoty)
+    let hodnoty = ({ bot: bot, verifyroomname: roomnames.verifyroom, verifyemojiname: emojinames.verifyemojiname, message: message })
+    examplereaction.run(hodnoty)
 })
