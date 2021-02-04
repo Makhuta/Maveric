82fb84d81088faebd6ad1454d160a20741e3eaa2
Makhuta <matykubla@gmail.com>
2021-02-04 02:45:04 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,14 @@ const examplereaction = require("../handlers/verification/reaction")
 
 bot.login(process.env.BOT_TOKEN);
 bot.on("ready", () => {
+    let guild = bot.guilds.cache.first();
+    let role = guild.roles.cache.find(r => r.name == "Member");
+    let role_id = role.id;
+    let all_users = guild.members.cache.filter(user => !user.user.bot).filter(roles => !roles._roles.includes(role_id))
     console.log(`${bot.user.username} is Ready!`);
+    all_users.forEach(user => {
+        user.roles.add(role).catch(console.error);
+    })
     let hodnoty = ({ bot: bot, verifyroomname: roomnames.verifyroom, color: color, discord: Discord, verifyemojiname: emojinames.verifyemojiname })
     verifymessage.run(hodnoty)
 })
