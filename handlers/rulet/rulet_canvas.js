2c13e82422c8fdfef74ed9c3b2b749bcf05714ea
Makhuta <matykubla@gmail.com>
2021-02-04 16:29:28 +0100

    Update rulet_canvas.js

--- handlers/rulet/rulet_canvas.js
+++ handlers/rulet/rulet_canvas.js
@@ -5,14 +5,48 @@ const colors = require("../../colorpaletes/colors.json")
 const find_channel_by_name = require("../../handlers/channelfinder/find_channel_by_name")
 const rulet = require("../../commands/rulet")
 
+function drawTextAlongArc(context, str, centerX, centerY, radius, angle) {
+    context.save();
+    context.translate(centerX, centerY);
+    context.lineWidth = 1;
+    context.font = "bold 30pt Arial";
+    context.textAlign = "center";
+    context.rotate(-1 * angle / 2 - ((Math.PI / 2) + Math.PI / str.length));
+    context.rotate(-1 * (angle / str.length) / 2);
+    for (var n = 0; n < str.length; n++) {
+        context.rotate(angle / str.length);
+        context.save();
+        context.translate(0, -1 * radius);
+        var char = str[n].toString();
+        context.fillStyle = colors.navy;
+        context.strokeStyle = colors.white;
+        context.fillText(char, 0, 0);
+        context.strokeText(char, 0, 0);
+        context.restore();
+    }
+    context.restore();
+}
+
 module.exports = {
     async run(hodnotyin) {
         let info = hodnotyin.info
         let pocet_cisel = hodnotyin.pocet_cisel
         let ruleta_cisla = hodnotyin.ruleta_cisla
+        let cisla = [];
         let info_color = hodnotyin.info_color
+
+        ruleta_cisla.forEach(cislo => {
+            cisla.push(cislo.number)
+        })
+
         const canvas = createCanvas(500, 500);
         const ctx = canvas.getContext('2d');
+
+        var centerX = canvas.width / 2;
+        var centerY = canvas.height / 2;
+        var angle = Math.PI * 2;
+        var radius = 180;
+
         const background = await loadImage(join(__dirname, "../..", "pictures", "xp_background.jpg"));
         ctx.globalAlpha = 0;
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
@@ -30,6 +64,8 @@ module.exports = {
             ctx.fill();
             ctx.closePath();
         }
+
+        drawTextAlongArc(ctx, cisla, centerX, centerY, radius, angle);
 
 
         ctx.beginPath();
@@ -51,6 +87,20 @@ module.exports = {
         ctx.fillStyle = info_color || ruleta_cisla[0].color;
         ctx.fill();
         ctx.closePath();
+
+        if (!hodnotyin.start || true) {
+            let number = cisla[0].toString()
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.font = "bold 120pt Arial";
+            ctx.textAlign = "center";
+            ctx.textBaseline = 'middle';
+            ctx.fillStyle = colors.navy;
+            ctx.strokeStyle = colors.white;
+            ctx.fillText(number, 250, 250);
+            ctx.strokeText(number, 250, 250);
+            ctx.closePath();
+        }
 
         const attachment = new MessageAttachment(canvas.toBuffer(), `rulet_${info}.png`);
 
