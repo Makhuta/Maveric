dc6d142a12675b3dc3743ef123dbea05835c425e
Makhuta <matykubla@gmail.com>
2021-03-16 13:36:00 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -19,6 +19,17 @@ bot.on("message", async message => {
     if (commandfile === undefined) return require("@handlers/find_channel_by_name").run({ zprava: user_language.CMD_NOT_EXIST, roomname: message.channel.name, message: message });
     let rle = commandfile.help.accessableby
 
+
+
+    if (commandfile && rle.some(rl => message.member.roles.cache.has(message.guild.roles.cache.find(rla => rla.name === rl).id))) {
+        commandfile.run(message, args, botconfig, user_lang_role);
+        return
+    }
+
+
+    /*
+    message.member.roles.cache.has(message.guild.roles.cache.find(rla => rla.name === rle[r]).id)
+    
     for (var r = 0; r < rle.length; r++) {
         if (rle[r] === undefined) return;
         if (commandfile && message.member.roles.cache.has(message.guild.roles.cache.find(rla => rla.name === rle[r]).id)) {
@@ -27,7 +38,7 @@ bot.on("message", async message => {
         } else {
 
         }
-    }
+    }*/
     require("@handlers/find_channel_by_name").run({ zprava: user_language.NOT_HAVE_PERMISSION, roomname: message.channel.name, message: message });
 })
 
