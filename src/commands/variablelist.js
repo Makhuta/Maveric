aaf208cded170db443ccb4d7055558148ba8fa4c
Makhuta <matykubla@gmail.com>
2022-06-09 01:21:13 +0200

    Added commands changevariable and variablelist

---
+++ src/commands/variablelist.js
@@ -0,0 +1,48 @@
+const { join } = require("path");
+const { MessageEmbed } = require("discord.js");
+const DefaultFunctionsStates = require(join(
+  Configs,
+  "DefaultFunctionsStates.json"
+));
+
+module.exports = {
+  name: "VariableList",
+  description:
+    "This command will show you list of variable configs and their states.",
+  helpdescription:
+    "This command will show you list of variabl configs and their states.",
+  default: false,
+  usage: "/variablelist",
+  helpname: "Variable list",
+  type: "Global",
+  async run(interaction) {
+    let GuildID = interaction.guildId;
+    let ConfigList = GuildsConfigs[GuildID].config;
+    let fields = [];
+
+    for (f in DefaultFunctionsStates) {
+      fields.push({
+        name: DefaultFunctionsStates[f].name,
+        value: ConfigList[DefaultFunctionsStates[f].name]
+      });
+    }
+    const embed = new MessageEmbed()
+      .setTitle("Variable list")
+      .setColor(require(join(ColorPaletes, "colors.json")).red)
+      .addFields(fields)
+      .setTimestamp();
+
+    interaction.reply({
+      embeds: [embed],
+      ephemeral: true
+    });
+  },
+  async create({ commands, permissions }) {
+    let command = await commands?.create({
+      name: this.name.toLowerCase(),
+      description: this.description,
+      defaultPermission: permissions
+    });
+    return command;
+  }
+};
