257c929bfbf8d62b70442732508399309d6107c0
Makhuta <matykubla@gmail.com>
2022-06-12 23:20:44 +0200

    Add force register comamnd for reregistering commands

---
+++ src/commands/forceregister.js
@@ -0,0 +1,31 @@
+const { client } = require(DClientLoc);
+let { join } = require("path");
+let RegisterCommand = require(join(Functions, "RegisterCommand.js"));
+
+module.exports = {
+  name: "ForceRegister",
+  description: "This is the test command.",
+  default: "BotOwner",
+  helpdescription: "This is the test command.",
+  usage: "!forceregister",
+  helpname: "ForceRegister",
+  type: "Testing",
+  async run(message, args) {
+    let cmds = message.guild.commands;
+    let requrestedcmd = CommandList.find((c) => c.Name == args[0]);
+    let requrestedcmdexist = requrestedcmd != undefined;
+
+    if (!requrestedcmdexist) {
+      return message.reply({ content: `${args[0]} not exist!` });
+    }
+    if (requrestedcmd.Type == "Testing" || requrestedcmd.Type == "Disabled") {
+      return message.reply({
+        content: `Registering for ${args[0]} was disabled!`
+      });
+    }
+    RegisterCommand({ cmds, NRCMD: { FileName: requrestedcmd.FileName } });
+    message.reply({
+      content: `${args[0]} has been force registered!`
+    });
+  }
+};
