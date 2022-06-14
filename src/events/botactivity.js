3a571264ead9881d5ce7219c64a422c84aa95385
Makhuta <matykubla@gmail.com>
2022-06-14 20:37:04 +0200

    Changing server invite to env variable

--- src/events/botactivity.js
+++ src/events/botactivity.js
@@ -1,12 +1,13 @@
 const { client, NSBR } = require(DClientLoc);
+require("dotenv").config();
 
-var typ_aktivity = [`Managing $NumberOfGuilds servers.`, "discord.gg/N7fxaAC"];
+var typ_aktivity = [`Managing $NumberOfGuilds servers.`, process.env.NSBR_SERVER_INVITE];
 
 async function aktivita() {
   let NumberOfGuilds = (await client.guilds.fetch()).size;
   let aktivita = typ_aktivity.shift();
 
-  aktivita = aktivita.replace("$NumberOfGuilds", NumberOfGuilds);
+  aktivita = aktivita?.replace("$NumberOfGuilds", NumberOfGuilds);
 
   client.user.setActivity(aktivita, { type: "PLAYING" });
   typ_aktivity.push(aktivita);
