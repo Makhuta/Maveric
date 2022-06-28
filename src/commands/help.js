6ecd8fbf81aea2c19795444a40b9b262920a1e98
Makhuta <matykubla@gmail.com>
2022-06-28 19:38:15 +0200

    Edit
    Changed embed from scratch and added category

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,5 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 const { join } = require("path");
+const { isUndefined } = require("util");
+const { client } = require(DClientLoc);
 
 module.exports = {
   name: "Help",
@@ -9,81 +11,101 @@ module.exports = {
   usage: "/help",
   helpname: "Help",
   type: "Global",
+  category: "Main",
   PMEnable: true,
   async run(interaction) {
-    const { options } = interaction;
-    let guildID = interaction.guildId;
-    let RequestedCommandName = options.getString("name");
-    let RequestIsUndefined = RequestedCommandName == null;
+    interaction[
+      "url"
+    ] = `https://discord.com/channels/${interaction.guild.id}/${interaction.channel.id}/${interaction.id}`;
+    let cmds = {};
+    let CommandNameLength = {};
 
-    if (!RequestIsUndefined) {
-      let cmd = require(CommandList.find((c) => c.Name == RequestedCommandName)
-        .Location);
-      let fields = [
+    for (c in CommandList) {
+      if (CommandList[c].Type != "Testing") {
+        let cmd = require(CommandList[c].Location);
+        let CommandCategory = cmd.category ? cmd.category : "Other";
+        if (!Object.keys(cmds).includes(CommandCategory)) {
+          cmds[CommandCategory] = [];
+          CommandNameLength[CommandCategory] = 0;
+        }
+
+        let CMDPopup = `Usage: ${cmd.usage}\nDescription: ${cmd.helpdescription}`
+
+        if (
+          cmd.helpname.length <= 10 &&
+          CommandNameLength[CommandCategory] <= 10
+        ) {
+          cmds[CommandCategory].push(
+            `[\`${cmd.helpname}\`](${interaction.url} "${CMDPopup}")`
+          );
+          CommandNameLength[CommandCategory] = +cmd.helpname.length;
+        } else {
+          cmds[CommandCategory].push(
+            `[\`${cmd.helpname}\`](${interaction.url} "${CMDPopup}")\n`
+          );
+          CommandNameLength[CommandCategory] = cmd.helpname.length;
+        }
+      }
+    }
+
+    var embed = new MessageEmbed()
+      .setAuthor({
+        name: `Available commands`,
+        iconURL: client.user.displayAvatarURL()
+      })
+      .setDescription(`Brackets in command usage means different things \`(option)\` means that it is optional \`[option]\` means than it is required.`)
+      .setColor(require(join(ColorPaletes, "colors.json")).red)
+      .setThumbnail("attachment://question_mark.png")
+      .setFooter({
+        text: "Hover over commands for info!"
+      })
+      .setTimestamp();
+    if (isUndefined(cmds["Other"]) || cmds["Other"].length < 1) {
+      embed.addFields(
+        { name: "Main", value: cmds["Main"].join(" "), inline: true },
         {
-          name: "Description",
-          value: cmd.helpdescription
+          name: "Moderation",
+          value: cmds["Moderation"].join(" "),
+          inline: true
         },
         {
-          name: "Usage",
-          value: cmd.usage
+          name: "Music",
+          value: cmds["Music"].join(" "),
+          inline: true
         }
-      ];
-      var embed = new MessageEmbed()
-        .setTitle(`${cmd.helpname}`)
-        .setColor(require(join(ColorPaletes, "colors.json")).red)
-        .addFields(fields)
-        .setTimestamp();
-
-      interaction.reply({
-        embeds: [embed],
-        ephemeral: true
-      });
+      );
     } else {
-      let cmds = [];
-      for (c in CommandList) {
-        if (CommandList[c].Type != "Testing") {
-          let cmd = require(CommandList[c].Location);
-          cmds.push(cmd.helpname);
-        }
-      }
-      console.info(cmds.join("\n"));
-      var embed = new MessageEmbed()
-        .setTitle(`Available commands`)
-        .setColor(require(join(ColorPaletes, "colors.json")).red)
-        .addField("Commands", cmds.join("\n"))
-        .setTimestamp();
-
-      interaction.reply({
-        embeds: [embed],
-        ephemeral: true
-      });
+      embed.addFields(
+        { name: "Main", value: cmds["Main"].join(" "), inline: true },
+        {
+          name: "Moderation",
+          value: cmds["Moderation"].join(" "),
+          inline: true
+        },
+        {
+          name: "Music",
+          value: cmds["Music"].join(" "),
+          inline: true
+        },
+        { name: "Other", value: cmds["Other"].join(" "), inline: false }
+      );
     }
+    embed.addField(
+      `Consider voting for me to unlock more features.`,
+      `[TOP.GG](https://top.gg/bot/${process.env.TOPGGID}/vote)`
+    );
+
+    interaction.reply({
+      embeds: [embed],
+      files: [join(Pictures, "question_mark.png")],
+      ephemeral: true
+    });
   },
   async create({ commands, permissions }) {
-    let choices = [];
-    for (f in CommandList) {
-      if (CommandList[f].Type != "Testing") {
-        choices.push({
-          name: CommandList[f].Name,
-          value: CommandList[f].Name
-        });
-      }
-    }
-    let options = [
-      {
-        name: "name",
-        description: "Name of command you want to help with",
-        required: false,
-        type: CommandTypes.STRING,
-        choices: choices
-      }
-    ];
     let command = await commands?.create({
       name: this.name.toLowerCase(),
       description: this.description,
-      defaultPermission: permissions,
-      options
+      defaultPermission: permissions
     });
     return command;
   }
