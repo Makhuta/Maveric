5d82f5dcef799a7c121d4fab398d18eb0cb67b5d
Makhuta <matykubla@gmail.com>
2022-08-19 21:55:37 +0200

    Add
    Added server stats webhook to support server

--- src/events/guildMapcreator.js
+++ src/events/guildMapcreator.js
@@ -1,6 +1,40 @@
 global.BotGuilds = new Map();
 
 const { client, NSBR } = require(DClientLoc);
+const { MessageBuilder, Webhook } = require("discord-webhook-node");
+
+const ServerStatsHook = new Webhook(process.env.SERVER_STATS_WEBHOOK);
+
+async function StatsEmbedCreator({ guild, add }) {
+  let { id, name, icon, memberCount, joinedTimestamp, ownerId } = guild;
+  let GuildInfo = {
+    ID: id,
+    Name: name,
+    Icon: icon,
+    IconURL: icon ? `https://cdn.discordapp.com/icons/${id}/${icon}.webp?size=512` : `https://cdn.discordapp.com/embed/avatars/1.png`,
+    MamberCount: memberCount,
+    JoinedTimestamp: Math.round(joinedTimestamp / 1000),
+    OwnerID: ownerId,
+    GuildNumber: client.guilds.cache.size
+  };
+
+  let embed = new MessageBuilder()
+    .setTitle(`Server ${add ? "added" : "removed"}`)
+    .setDescription(`${client.user.username} has been ${add ? "added to" : "removed from"} server: \`${GuildInfo.Name}\``)
+    .setColor("RANDOM")
+    .setThumbnail(GuildInfo.IconURL)
+    .addField("Server name:", `\`${GuildInfo.Name}\``, true)
+    .addField("Server ID:", GuildInfo.ID, true)
+    .addField("Member count:", GuildInfo.MamberCount, true)
+    .addField("Server owner:", `<@${GuildInfo.OwnerID}>`, true)
+    .addField(`${client.user.username} is watching:`, `${GuildInfo.GuildNumber} servers`, true)
+    .addField("Joined on:", `<t:${GuildInfo.JoinedTimestamp}:D>`, true)
+    .setThumbnail(GuildInfo.IconURL)
+    .setFooter(client.user.username, client.user.displayAvatarURL())
+    .setTimestamp()
+
+  return embed;
+}
 
 NSBR.on("ready", () => {
   client.guilds
@@ -15,10 +49,14 @@ NSBR.on("ready", () => {
     });
 });
 
-client.on("guildCreate", (guild) => {
+client.on("guildCreate", async (guild) => {
   BotGuilds.set(guild.id, {});
+  let embed = await StatsEmbedCreator({ guild, add: true });
+  ServerStatsHook.send(embed);
 });
 
-client.on("guildDelete", (guild) => {
+client.on("guildDelete", async (guild) => {
   BotGuilds.delete(guild.id);
+  let embed = await StatsEmbedCreator({ guild, add: false });
+  ServerStatsHook.send(embed);
 });
