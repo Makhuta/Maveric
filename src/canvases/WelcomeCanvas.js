ba34072520a3dad978b952063edaf4205c300b2d
Makhuta <matykubla@gmail.com>
2022-08-24 10:58:58 +0200

    Fix
    Fixed issue that bot crashed if not having permissions

--- src/canvases/WelcomeCanvas.js
+++ src/canvases/WelcomeCanvas.js
@@ -5,11 +5,12 @@ const color = require(join(ColorPaletes, "colors.json"));
 
 module.exports = {
   run: async (hodnoty) => {
-    let message = hodnoty.channel;
+    let channel = hodnoty.channel;
     let target = hodnoty.target;
     let user_username = target.username;
     let stav = hodnoty.stav;
     let datum = hodnoty.datum;
+    let guild = channel.guild
 
     if (user_username.length >= 20) {
       user_username = user_username.slice(0, 20) + "...";
@@ -50,76 +51,41 @@ module.exports = {
     ctx.fill();
 
     ctx.fillStyle = color.dark_gray;
-    ctx.fillRect(
-      vyska / 2 + polomer + vzdalenost_od_avataru,
-      vyska / 2 - sirkapole - vzdalenost1,
-      sirka - (vyska / 2 + polomer + vzdalenost_od_avataru) - 40,
-      sirkapole
-    );
+    ctx.fillRect(vyska / 2 + polomer + vzdalenost_od_avataru, vyska / 2 - sirkapole - vzdalenost1, sirka - (vyska / 2 + polomer + vzdalenost_od_avataru) - 40, sirkapole);
     ctx.fill();
 
     ctx.textAlign = "left";
     ctx.lineWidth = sirkacary;
     ctx.font = `80px ${stav_font}`;
-    ctx.strokeText(
-      stav,
-      vyska / 2 + polomer + vzdalenost_od_avataru + text_mensi_o,
-      110
-    );
+    ctx.strokeText(stav, vyska / 2 + polomer + vzdalenost_od_avataru + text_mensi_o, 110);
 
     ctx.fillStyle = barvy[stav_barva];
     ctx.textAlign = "left";
     ctx.font = `lighter 80px ${stav_font}`;
-    ctx.fillText(
-      stav,
-      vyska / 2 + polomer + vzdalenost_od_avataru + text_mensi_o,
-      110
-    );
+    ctx.fillText(stav, vyska / 2 + polomer + vzdalenost_od_avataru + text_mensi_o, 110);
 
     ctx.fillStyle = color.white;
     ctx.textAlign = "left";
     ctx.font = `${sirkapole - velikost_textu}px Arial Black`;
-    ctx.fillText(
-      user_username,
-      vyska / 2 + polomer + vzdalenost_od_avataru + text_mensi_o,
-      vyska / 2 - vzdalenost1 - text_mensi_o
-    );
+    ctx.fillText(user_username, vyska / 2 + polomer + vzdalenost_od_avataru + text_mensi_o, vyska / 2 - vzdalenost1 - text_mensi_o);
 
     ctx.fillStyle = color.dark_gray;
-    ctx.fillRect(
-      vyska / 2 + polomer + vzdalenost_od_avataru,
-      vyska / 2 + vzdalenost1,
-      190,
-      sirkapole
-    );
+    ctx.fillRect(vyska / 2 + polomer + vzdalenost_od_avataru, vyska / 2 + vzdalenost1, 190, sirkapole);
     ctx.fill();
 
     ctx.fillStyle = color.white;
     ctx.textAlign = "left";
     ctx.font = `${sirkapole - velikost_textu}px Arial Black`;
-    ctx.fillText(
-      "# " + target.discriminator,
-      vyska / 2 + polomer + vzdalenost_od_avataru + text_mensi_o,
-      vyska / 2 + sirkapole + vzdalenost1 - text_mensi_o
-    );
+    ctx.fillText("# " + target.discriminator, vyska / 2 + polomer + vzdalenost_od_avataru + text_mensi_o, vyska / 2 + sirkapole + vzdalenost1 - text_mensi_o);
 
     ctx.fillStyle = color.dark_gray;
-    ctx.fillRect(
-      vyska / 2 + polomer - 60,
-      vyska - 30 - sirkapole,
-      sirka - (vyska / 2 + polomer - 60) - 40,
-      sirkapole
-    );
+    ctx.fillRect(vyska / 2 + polomer - 60, vyska - 30 - sirkapole, sirka - (vyska / 2 + polomer - 60) - 40, sirkapole);
     ctx.fill();
 
     ctx.fillStyle = color.white;
     ctx.textAlign = "left";
     ctx.font = `${sirkapole - velikost_textu}px Arial Black`;
-    ctx.fillText(
-      `${verze_pro_datum[stav_barva]}: ` + datum,
-      vyska / 2 + polomer + text_mensi_o - 60,
-      vyska - 42
-    );
+    ctx.fillText(`${verze_pro_datum[stav_barva]}: ` + datum, vyska / 2 + polomer + text_mensi_o - 60, vyska - 42);
 
     ctx.globalAlpha = 1;
     ctx.arc(vyska / 2, vyska / 2, polomer, 0, Math.PI * 2, true);
@@ -128,21 +94,20 @@ module.exports = {
     ctx.stroke();
     ctx.closePath();
     ctx.clip();
-    const avatar = await loadImage(
-      target.displayAvatarURL({ format: "jpg", size: rozliseni })
-    );
-    ctx.drawImage(
-      avatar,
-      vyska / 2 - polomer - sirkacary,
-      vyska / 2 - polomer - sirkacary,
-      256,
-      256
-    );
+    const avatar = await loadImage(target.displayAvatarURL({ format: "jpg", size: rozliseni }));
+    ctx.drawImage(avatar, vyska / 2 - polomer - sirkacary, vyska / 2 - polomer - sirkacary, 256, 256);
 
-    const attachment = new MessageAttachment(
-      canvas.toBuffer(),
-      `${obraz_jmeno[stav_barva]}.png`
-    );
-    message.send({ files: [attachment] });
+    const attachment = new MessageAttachment(canvas.toBuffer(), `${obraz_jmeno[stav_barva]}.png`);
+    channel.send({ files: [attachment] }).catch((error) => {
+      InfoHandler["MemberJoinError"] = {};
+      if (InfoHandler["MemberJoinError"][guild.id] == undefined) {
+        InfoHandler["MemberJoinError"][guild.id] = [];
+      }
+      InfoHandler["MemberJoinError"][guild.id].push({
+        ErrorMessage: error,
+        guildID: guild.id
+      });
+      GuildsConfigs[guild.id]["config"]["WELCOMERENABLEDERRORED"] = "false";
+    });
   }
 };
