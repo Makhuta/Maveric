c63d8e68ee245d0bbe4e71001ce0b2383f865225
Makhuta <matykubla@gmail.com>
2023-01-28 20:51:05 +0100

    Update
    Updated dashboard website

--- website/index.js
+++ website/index.js
@@ -3,19 +3,37 @@ const util = require('util')
 const {join} = require("path");
 const express = require("express")
 const hbs = require("express-handlebars");
+const Handlebars = require("handlebars");
 const app = express()
 
 var port = process.env.PORT || 8080
 var host = process.env.HOST
 
-var layoutslozka = __dirname + "/views/layouts"
+var layoutsDir = __dirname + "/views/layouts"
+var partialsDir = __dirname + "/views/partials"
 
 
 app.engine("hbs", hbs.create({
     extname: "hbs",
     defaultLayout: "layout",
-    layoutsDir: layoutslozka
+    layoutsDir,
+    partialsDir
 }).engine)
+
+Handlebars.registerHelper('if_has', function (array) {
+    return array.length > 0;
+}
+)
+
+Handlebars.registerHelper('is_type', function (type, requested_type) {
+    return type == requested_type;
+}
+)
+
+Handlebars.registerHelper('array_length', function (array) {
+    return array.length;
+}
+)
 
 app.set("view engine", "hbs")
 app.set("views", join(__dirname, "/views"))
--- website/load_guilds.js
+++ website/load_guilds.js
@@ -1,6 +1,7 @@
 const { client } = require(DClientLoc);
 const {join} = require("path");
 const util = require('util')
+const PermissionToArray = require(join(Functions, "global/PermissionToArray.js"));
 
 
 function timeConverterJSON(UNIX_timestamp) {
@@ -17,68 +18,110 @@ function timeConverterJSON(UNIX_timestamp) {
 
 async function parsed_config(guild_id) {
     let config = GuildsConfigs[guild_id].config;
-    return [
-        {
-            name: "Gate category",
-            value: config.GATECATEGORY
-        },
-        {
-            name: "Gate channel",
-            value: config.GATEROOM
-        },
-        {
-            name: "Server stats category",
-            value: config.SERVERSTATS
-        },
-        {
-            name: "Member count channel",
-            value: config.MEMBERCOUNT
-        },
-        {
-            name: "Online count channel",
-            value: config.ONLINECOUNT
-        },
-        {
-            name: "Offline count channel",
-            value: config.OFFLINECOUNT
-        },
-        {
-            name: "Bot advertisement",
-            value: config.BOTADVERTISMENT ? "True" : "False"
-        },
-        {
-            name: "Counter enabled",
-            value: config.COUNTERENABLED ? "True" : "False"
-        },
-        {
-            name: "Online counter enabled",
-            value: config.ONLINE_COUNTER_ENABLED ? "True" : "False"
-        },
-        {
-            name: "Offline counter enabled",
-            value: config.OFFLINE_COUNTER_ENABLED ? "True" : "False"
-        },
-        {
-            name: "Member counter enabled",
-            value: config.MEMBERS_COUNTER_ENABLED ? "True" : "False"
-        },
-        {
-            name: "Stats category enabled",
-            value: config.STATS_CATEGORY_ENABLED ? "True" : "False"
-        },
-        {
-            name: "Money enabled",
-            value: config.MONEYENABLED ? "True" : "False"
-        },
-        {
-            name: "Welcome message enabled",
-            value: config.WELCOME_MESSAGE_ENABLED ? "True" : "False"
-        },
-        {
-            name: "Welcomer enabled",
-            value: config.WELCOMERENABLED ? "True" : "False"
-        },
-    ];
+    let output = {};
+
+    for(key of Object.keys(config)) {
+        value = config[key] == "" ? "undefined" : config[key] == true ? "True" : config[key] == false ? "False" : config[key];
+        output[key] = {
+            name: key,
+            value: value
+        };
+    }
+    return output;
+}
+
+async function make_user(user_list, user_obj, roles_list) {
+    let joined_date = timeConverterJSON(user_obj.joinedTimestamp);
+    let user_roles = {};
+    for(role of user_obj.roles.cache) {
+        user_roles[role[0]] = roles_list[role[0]];
+    }
+    user_list[user_obj.user.id] = {
+        id: user_obj.user.id,
+        username: user_obj.user.username,
+        discriminator: user_obj.user.discriminator,
+        nickname: user_obj.nickname ? user_obj.nickname : "NULL",
+        roles: Object.values(user_roles),
+        joinedTimestamp: user_obj.joinedTimestamp,
+        joined_date: `${joined_date.date}.${joined_date.month}.${joined_date.year} ${joined_date.hour}:${joined_date.min}`,
+        avatar_url: user_obj.displayAvatarURL()
+    }
+}
+
+function ChannelList(guild) {
+    let TextChannels = [];
+    let VoiceChannels = [];
+    let CategoryChannels = [];
+    let Structured = {};
+    let All = {};
+
+    for (ch of guild.channels.cache) {
+        ch = ch[1];
+        //Voice channel
+        if (ch.type == 2) {
+        All[ch.id] = {
+            id: ch.id,
+            name: ch.name,
+            type: "Voice channel",
+            parent_id: ch.parentId,
+            raw_position: ch.rawPosition,
+            bitrate: ch.bitrate,
+            user_limit: ch.userLimit
+        };
+        VoiceChannels.push(All[ch.id]);
+        } 
+        //Text channel
+        else if (ch.type == 0) {
+        All[ch.id] = {
+            id: ch.id,
+            name: ch.name,
+            type: "Text channel",
+            parent_id: ch.parentId,
+            raw_position: ch.rawPosition
+        };
+        TextChannels.push(All[ch.id]);
+        } 
+        //Channel category
+        else if (ch.type == 4) {
+        All[ch.id] = {
+            id: ch.id,
+            name: ch.name,
+            type: "Channel category",
+            raw_position: ch.rawPosition
+        };
+        CategoryChannels.push(All[ch.id]);
+        }
+    }
+
+    for(c of CategoryChannels) {
+        Structured[c.id] = {
+            id: c.id,
+            name: c.name,
+            type: c.type,
+            raw_position: c.rawPosition,
+            channels: [...VoiceChannels.filter(channel => channel.parent_id == c.id), ...TextChannels.filter(channel => channel.parent_id == c.id)]
+        };
+    }
+
+    return {TextChannels, VoiceChannels, CategoryChannels, Structured, All};
+}
+
+async function parse_invites(guild, channel_list, members_list) {
+    let invite_list = {};
+    for(invite of (await guild.invites.fetch()).values()) {
+        let invite_created_at = timeConverterJSON(invite.createdTimestamp);
+        invite_list[invite.code] = {
+            code: invite.code,
+            max_age: invite.maxAge,
+            uses: invite.uses,
+            max_uses: invite.maxUses,
+            inviter: [members_list[invite.inviterId]],
+            channels: [channel_list.All[invite.channel.id]],
+            created_timestamp: invite.createdTimestamp,
+            created_at: `${invite_created_at.date}.${invite_created_at.month}.${invite_created_at.year} ${invite_created_at.hour}:${invite_created_at.min}`,
+        }
+    }
+    return invite_list;
 }
 
 
@@ -88,54 +131,53 @@ module.exports = {
         let guids = client.guilds.cache;
         for(guild of guids) {
             guild = guild[1];
-            let members_array = [];
-            let bots_array = [];
-            let members_count = 0;
-            let bots_count = 0;
-            let has_members = false;
-            let has_bots = false;
+            let roles_list = {};
+            let roles = guild.roles.cache;
+            for(role of roles) {
+                role = role[1];
+                roles_list[role.id] = {
+                    id: role.id,
+                    name: role.name,
+                    rawpos: role.rawPosition,
+                    managed: role.managed,
+                    mentionable: role.mentionable,
+                    permissions: PermissionToArray(role.permissions)
+                }
+            }
+
+
+
+
+
+            let members_list = {};
+            let bot_list = {};
             let members = guild.members.cache;
             for(member of members) {
                 member = member[1];
-                let joined_date = timeConverterJSON(member.joinedTimestamp);
                 if(member.user.bot) {
-                    has_bots = true;
-                    bots_count++;
-                    bots_array.push({
-                        id: member.user.id,
-                        name: member.user.username,
-                        joinedTimestamp: member.joinedTimestamp,
-                        joined_date: `${joined_date.date}.${joined_date.month}.${joined_date.year} ${joined_date.hour}:${joined_date.min}`
-                    })
+                    make_user(bot_list, member, roles_list);
                 } else {
-                    has_members = true;
-                    members_count++;
-                    members_array.push({
-                        id: member.user.id,
-                        name: member.user.username,
-                        nickname: member.nickname ? member.nickname : "NULL",
-                        discriminator: member.user.discriminator,
-                        joinedTimestamp: member.joinedTimestamp,
-                        joined_date: `${joined_date.date}.${joined_date.month}.${joined_date.year} ${joined_date.hour}:${joined_date.min}`
-                    })
+                    make_user(members_list, member, roles_list);
                 }
-                //console.info(util.inspect(member, false, null, true));
             }
+
+            let guild_created_at = timeConverterJSON(guild.createdAt)
+            let channels_list = ChannelList(guild);
 
             output.push({
                 id : guild.id,
                 name: guild.name,
-                bot_count: bots_count,
-                member_count: members_count,
-                has_bots,
-                has_members,
-                bots: bots_array,
-                members: members_array,
-                config: await parsed_config(guild.id)
+                members: Object.values(members_list),
+                bots: Object.values(bot_list),
+                roles: Object.values(roles_list),
+                channels: Object.values(channels_list.Structured),
+                invites: Object.values(await parse_invites(guild, channels_list, members_list)),
+                created_at: `${guild_created_at.date}.${guild_created_at.month}.${guild_created_at.year} ${guild_created_at.hour}:${guild_created_at.min}`,
+                owner: [members_list[guild.ownerId]],
+                config: Object.values(await parsed_config(guild.id))
             });
         }
         
-
         return output;
     }
 }
